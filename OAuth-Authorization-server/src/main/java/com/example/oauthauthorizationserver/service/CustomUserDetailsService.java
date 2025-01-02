9d2df502959124159d1fef1e121096461a32738b
teaMan77 <180627854+teaMan77@users.noreply.github.com>
2025-01-02 18:47:38 +0530

    Authorization server configuration

--- OAuth-Authorization-server/src/main/java/com/example/oauthauthorizationserver/service/CustomUserDetailsService.java
+++ OAuth-Authorization-server/src/main/java/com/example/oauthauthorizationserver/service/CustomUserDetailsService.java
@@ -3,11 +3,14 @@ package com.example.oauthauthorizationserver.service;
 import com.example.oauthauthorizationserver.entity.User;
 import com.example.oauthauthorizationserver.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -22,6 +25,11 @@ public class CustomUserDetailsService implements UserDetailsService {
 
     @Autowired
     private UserRepository userRepository;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(12);
+    }
 
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
