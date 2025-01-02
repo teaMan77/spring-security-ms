4b124d9cd9aa7aeb7a62d6b78cb7c7f8fc97f6af
teaMan77 <180627854+teaMan77@users.noreply.github.com>
2025-01-02 19:30:00 +0530

    Implementing custom authentication provider and binding it with default security filter

--- OAuth-Authorization-server/src/main/java/com/example/oauthauthorizationserver/config/DefaultSecurityConfig.java
+++ OAuth-Authorization-server/src/main/java/com/example/oauthauthorizationserver/config/DefaultSecurityConfig.java
@@ -1,13 +1,21 @@
 package com.example.oauthauthorizationserver.config;
 
+import com.example.oauthauthorizationserver.service.CustomAuthenticationProvider;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
+@Configuration
 @EnableWebSecurity
 public class DefaultSecurityConfig {
+
+    @Autowired
+    private CustomAuthenticationProvider customAuthenticationProvider;
 
     @Bean
     public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http)
@@ -20,5 +28,10 @@ public class DefaultSecurityConfig {
                 .formLogin(Customizer.withDefaults());
 
         return http.build();
+    }
+
+    @Autowired
+    public void bindAuthenticationProvider(AuthenticationManagerBuilder authenticationManagerBuilder) {
+        authenticationManagerBuilder.authenticationProvider(customAuthenticationProvider);
     }
 }
