3d9b36aaa2f2dbcc3878f8eb3a6aa8c72f41bbf9
teaMan77 <180627854+teaMan77@users.noreply.github.com>
2025-01-02 17:50:19 +0530

    Creating custom user details class to load users during authentication

---
+++ OAuth-Authorization-server/src/main/java/com/example/oauthauthorizationserver/service/CustomUserDetailsService.java
@@ -0,0 +1,54 @@
+package com.example.oauthauthorizationserver.service;
+
+import com.example.oauthauthorizationserver.entity.User;
+import com.example.oauthauthorizationserver.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@Service
+@Transactional
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findUserByEmail(email);
+
+        if (user == null) {
+            throw new UsernameNotFoundException("User Not Found...");
+        }
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getEmail(),
+                user.getPassword(),
+                user.isEnabled(),
+                true,
+                true,
+                true,
+                getAuthorities(List.of(user.getRole()))
+        );
+    }
+
+    private Collection<? extends GrantedAuthority> getAuthorities(List<String> roles) {
+        Set<GrantedAuthority> authorities = new HashSet<>();
+
+        for (String role : roles) {
+            authorities.add(new SimpleGrantedAuthority(role));
+        }
+
+        return authorities;
+    }
+}
