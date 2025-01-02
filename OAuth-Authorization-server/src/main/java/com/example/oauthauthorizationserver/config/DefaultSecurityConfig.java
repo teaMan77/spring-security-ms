9cae7eb56fc9a0a2c6b7f0e19caa696cffbdfb36
teaMan77 <180627854+teaMan77@users.noreply.github.com>
2025-01-02 19:11:17 +0530

    Auth server config - 2

---
+++ OAuth-Authorization-server/src/main/java/com/example/oauthauthorizationserver/config/DefaultSecurityConfig.java
@@ -0,0 +1,24 @@
+package com.example.oauthauthorizationserver.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+public class DefaultSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http)
+            throws Exception {
+        http.authorizeHttpRequests((authorize) -> authorize
+                        .anyRequest().authenticated()
+                )
+                // Form login handles the redirect to the login page from the
+                // authorization server filter chain
+                .formLogin(Customizer.withDefaults());
+
+        return http.build();
+    }
+}
